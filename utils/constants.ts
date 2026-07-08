b2354b6148fd9d3af7fccaa8dd13c9288859c2db
Debasis Nishank <nishankdebasis4@gmail.com>
2026-07-08 07:51:56 +0530

    Add all Indian states and an "Other" option for soil type/water source
    The farm signup forms only listed ~10-12 states (missing Odisha, West
    Bengal, and most others), and soil type/water source were closed
    dropdowns with no way to enter a value that wasn't listed.

--- utils/constants.ts
+++ utils/constants.ts
@@ -19,6 +19,46 @@ export const SUPPORTED_LANGUAGES = [
 
 export const DEFAULT_LANGUAGE = "en";
 
+// All Indian states and union territories
+export const INDIAN_STATES = [
+  "Andaman and Nicobar Islands",
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chandigarh",
+  "Chhattisgarh",
+  "Dadra and Nagar Haveli and Daman and Diu",
+  "Delhi",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jammu and Kashmir",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Ladakh",
+  "Lakshadweep",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Puducherry",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal",
+] as const;
+
 // Comprehensive list of crops grown in India, organized by category
 export const CROP_CATEGORIES = [
   {
